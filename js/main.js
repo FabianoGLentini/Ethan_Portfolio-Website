4ecf8017542c3b347ebf8f548fce02bdfef7a662
FabianoGLentini <fabiano.g.lentini@gmail.com>
2023-10-26 10:35:11 -0400

    Add: new font, adjust font sizing for project display

---
+++ js/main.js
@@ -0,0 +1,13 @@
+/******************************************
+/* PARALLAX
+/*******************************************/
+
+// const parallax = document.getElementById("parallax") 
+
+
+// window.addEventListener("scroll", paralaxScroll)
+
+// function paralaxScroll(){
+//     let offset = window.pageYOffset;
+//     parallax.style.backgroundPositionY = offset * 0.7 +"px"
+// }
