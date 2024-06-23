291ca124eaf476dd55a545b6c2d43ab84d69f349
FabianoGLentini <fabiano.g.lentini@gmail.com>
2024-06-23 18:07:40 -0400

    Add WIP: nav menu phone toggle x and hamburger on click

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
+
+
+
+/******************************************
+/* Nav Bar Toggle
+/*******************************************/
+
+const primaryNav = document.querySelector(".nav-body")
+const navToggle = document.querySelector(".mobile-nav-toggle")
+
+navToggle.addEventListener("click", () => {
+    const visibility = primaryNav.getAttribute("data-visible")
+
+    if(visibility === "false"){
+        primaryNav.setAttribute("data-visible" , true)  
+        navToggle.setAttribute("aria-expanded", true)
+      }else{
+        primaryNav.setAttribute("data-visible" , false)  
+        navToggle.setAttribute("aria-expanded", false)
+      }
+
+})
+
+// Toggle nav menu bar off when click a link or outside menu bar
+
+document.addEventListener("click", e => {
+  if(!navToggle.contains(e.target)) {
+    primaryNav.setAttribute("data-visible" , false)  
+    navToggle.setAttribute("aria-expanded", false)
+  }
+})
+
+
+
 /******************************************
 /* PARALLAX
 /*******************************************/
@@ -16,5 +50,3 @@
 /******************************************
 /* EMAIL COPY
 /*******************************************/
-
-
