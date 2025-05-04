07e2c8293af60915d0e34d21fb60ef12288f0fa3
FabianoGLentini <fabiano.g.lentini@gmail.com>
2025-05-03 21:23:23 -0400

    implement: lazy loading

--- js/main.js
+++ js/main.js
@@ -29,24 +29,3 @@ document.addEventListener("click", e => {
     navToggle.setAttribute("aria-expanded", false)
   }
 })
-
-
-
-/******************************************
-/* PARALLAX
-/*******************************************/
-
-// const parallax = document.getElementById("parallax") 
-
-
-// window.addEventListener("scroll", paralaxScroll)
-
-// function paralaxScroll(){
-//     let offset = window.pageYOffset;
-//     parallax.style.backgroundPositionY = offset * 0.7 +"px"
-// }
-
-
-/******************************************
-/* EMAIL COPY
-/*******************************************/
