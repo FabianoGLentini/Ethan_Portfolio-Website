93635ef4048e871cb4db82cfef61bc8198db3f54
FabianoGLentini <fabiano.g.lentini@gmail.com>
2025-05-05 10:06:17 -0400

    Formatting: formatting code set up and indentations & refractor code

--- js/main.js
+++ js/main.js
@@ -3,44 +3,42 @@
 /*******************************************/
 
 // insure that all menu info and main logo in view on first load
-window.addEventListener("load", _ => {
-    
-    if(!sessionStorage.getItem("visited")){
-        document.getElementById("title-card").scrollIntoView({
-            behavior: "smooth",
-            block: "center"
-        })
-        sessionStorage.setItem("visited", true)
-
-    }
-})
-
+window.addEventListener("load", (_) => {
+  if (!sessionStorage.getItem("visited")) {
+    document.getElementById("title-card").scrollIntoView({
+      behavior: "smooth",
+      block: "center",
+    });
+    sessionStorage.setItem("visited", true);
+  }
+});
 
 /******************************************
 /* Nav Bar Toggle
 /*******************************************/
 
-const primaryNav = document.querySelector(".nav-body")
-const navToggle = document.querySelector(".mobile-nav-toggle")
+const primaryNav = document.querySelector(".nav-body");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+// Toggle nav bar visible on menu click
 
 navToggle.addEventListener("click", () => {
-    const visibility = primaryNav.getAttribute("data-visible")
+  const visibility = primaryNav.getAttribute("data-visible");
 
-    if(visibility === "false"){
-        primaryNav.setAttribute("data-visible" , true)  
-        navToggle.setAttribute("aria-expanded", true)
-      }else{
-        primaryNav.setAttribute("data-visible" , false)  
-        navToggle.setAttribute("aria-expanded", false)
-      }
-
-})
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
 
 // Toggle nav menu bar off when click a link or outside menu bar
 
-document.addEventListener("click", e => {
-  if(!navToggle.contains(e.target)) {
-    primaryNav.setAttribute("data-visible" , false)  
-    navToggle.setAttribute("aria-expanded", false)
+document.addEventListener("click", (e) => {
+  if (!navToggle.contains(e.target)) {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
   }
-})
+});
